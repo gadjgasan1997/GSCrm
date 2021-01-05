e6e729a41d33bf0e3c3f68b1404b18b71f1e65f6
Gasan <gadj.gasan1997@gmail.com>
2021-01-05 10:44:15 +0300

    Поменял схему БД

--- GSCrm/Migrations/20210105035122_05.01.20201.Designer.cs
+++ GSCrm/Migrations/20210105035122_05.01.20201.Designer.cs
@@ -4,14 +4,16 @@ using GSCrm.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GSCrm.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210105035122_05.01.20201")]
+    partial class _050120201
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GSCrm/Migrations/20210105035122_05.01.20201.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GSCrm.Migrations
+{
+    public partial class _050120201 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasRead",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasRead",
+                table: "UserNotifications",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasRead",
+                table: "UserNotifications");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasRead",
+                table: "Notifications",
+                type: "bit",
+                nullable: true);
+        }
+    }
+}
